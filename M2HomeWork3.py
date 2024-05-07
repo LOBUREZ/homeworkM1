461094ecae7ee8a93e14c58c12d6c25c7e699487
LOBUREZ <86012846+LOBUREZ@users.noreply.github.com>
2024-05-08 00:54:22 +0300

    Add files via upload

---
+++ M2HomeWork3.py
@@ -0,0 +1,9 @@
+cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in cars:
+    print('Я езжу на автомабиле марки', i)
+
+
+cars_count = 0
+while cars_count <= 150: 
+    cars_count += 10
+    print(cars_count)
