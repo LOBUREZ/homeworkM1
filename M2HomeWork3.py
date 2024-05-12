ed4705f4b4c5e89853d6ba4db0156866087f4ad1
LOBUREZ <86012846+LOBUREZ@users.noreply.github.com>
2024-05-12 11:29:55 +0300

    Update M2HomeWork3.py

--- M2HomeWork3.py
+++ M2HomeWork3.py
@@ -1,9 +1,8 @@
 cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+cars_count = 0
 for i in cars:
     print('Я езжу на автомабиле марки', i)
-
-
-cars_count = 0
-while cars_count <= 150: # задали условие, что цикл будет идти до 150
     cars_count += 10
-    print(cars_count)
+
+
+print(cars_count)
